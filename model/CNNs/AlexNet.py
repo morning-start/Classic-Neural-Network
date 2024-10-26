9660cd99575c6de0b075616abc4fb521305b83c5
MorningStart <morning-start@foxmail.com>
2024-10-26 20:07:34 +0800

    AlexNet

--- model/CNNs/AlexNet.py
+++ model/CNNs/AlexNet.py
@@ -2,16 +2,26 @@ import torch
 from torch import nn
 
 
-# LINK https://zh.d2l.ai/chapter_convolutional-modern/alexnet.html
+# LINK https://proceedings.neurips.cc/paper/2012/file/c399862d3b9d6b76c8436e924a68c45b-Paper.pdf
 class AlexNet(nn.Module):
     """
-    2012年，AlexNet横空出世。它首次证明了学习到的特征可以超越手工设计的特征。它一举打破了计算机视觉研究的现状。
-    AlexNet使用了8层卷积神经网络，并以很大的优势赢得了2012年ImageNet图像识别挑战赛。
+    AlexNet模型类，继承自nn.Module。
+
+    该模型包含特征提取部分(self.features)、平均池化部分(self.avgpool)和分类器部分(self.classifier)。
     """
 
-    def __init__(self):
+    def __init__(self, num_classes=10):
+        """
+        初始化AlexNet模型。
+
+        Args:
+            num_classes (int): 分类器的输出类别数量，默认为10。
+            由于这里使用 Fashion-MNIST，所以用类别数为10，而非论文中的1000
+        """
+
         super(AlexNet, self).__init__()
-        self.net = nn.Sequential(
+        # NOTE 定义特征提取部分，使用Sequential容器按顺序定义卷积层、激活函数层和池化层
+        self.features = nn.Sequential(
             # 这里使用一个11*11的更大窗口来捕捉对象。
             # 同时，步幅为4，以减少输出的高度和宽度。
             # 另外，输出通道的数目远大于LeNet
@@ -33,16 +43,21 @@ class AlexNet(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Flatten(),
-            # 这里，全连接层的输出数量是LeNet中的好几倍。使用dropout层来减轻过拟合
+        )
+        # # NOTE 定义分类器部分，使用Sequential容器按顺序定义全连接层、激活函数层和Dropout层
+        self.classifier = nn.Sequential(
+            # 全连接层的输出数量是LeNet中的好几倍。使用dropout层来减轻过拟合
             nn.Linear(6400, 4096),
             nn.ReLU(),
             nn.Dropout(p=0.5),
             nn.Linear(4096, 4096),
             nn.ReLU(),
             nn.Dropout(p=0.5),
-            # 最后是输出层。由于这里使用Fashion-MNIST，所以用类别数为10，而非论文中的1000
-            nn.Linear(4096, 10),
+            # 输出层
+            nn.Linear(4096, num_classes),
         )
 
-    def forward(self, x: torch.Tensor):
-        return self.net(x)
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
